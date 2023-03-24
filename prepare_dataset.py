1c7e1beb2ea0d2e40cb311493160ca033b280824
tkuo-tkuo <nmakerbsiwill@gmail.com>
2023-03-24 15:46:55 +0000

    v0.2.10 add data integrity check for transcripts

--- prepare_dataset.py
+++ prepare_dataset.py
@@ -8,22 +8,34 @@ from mutagen.mp3 import MP3
 import constants
 
 # TODO: add detection mechanism to avoid suspicious txt (repetitive word occurance)
-# TODO: check audio & transcript data integrity
 # TODO: generate jsonl files based on transcripts into one folder
 # TODO: upload environment.yml after this script is completed
 
 
 def main():
     load_dotenv()
-
-    # TODO: add detection mechanism to avoid suspicious txt (repetitive word occurance)
-    # 1. for loop to locate every transcript in audio_files/
-    # 2. for every transcript, translate as list
-
-    # TODO: check transcript
-
-    # check audio files
     check_audio_files()
+    check_transcripts()
+
+
+def check_transcripts():
+    audio_ids = os.listdir(constants.AUDIO_FILE_ROOT)
+    for audio_id in audio_ids:
+        audio_file_dir = f"{constants.AUDIO_FILE_ROOT}/{audio_id}"
+
+        # 1-minute transcripit preview 
+        _check_file_exist(
+            f"{audio_file_dir}/whisper/{constants.AudioFileKeyword.PREVIEW.value}.txt")
+
+        # 5-minutes transcripts 
+        total_length_in_milliseconds = _get_audio_length_in_milliseconds(
+            f"{audio_file_dir}/{constants.AudioFileKeyword.FULL.value}.mp3")
+        five_minutes_in_milliseconds = 5 * constants.ONE_MINUTE_IN_MILLISECONDS
+        upper_bound_index_hourly_chuck = math.ceil(
+            total_length_in_milliseconds/five_minutes_in_milliseconds)
+        for idx in range(1, upper_bound_index_hourly_chuck+1):
+            _check_file_exist(
+                f"{audio_file_dir}/whisper/{idx}{constants.AudioFileKeyword.FIVE_MINUTES_CHUCK.value}.txt")
 
 
 def check_audio_files():
