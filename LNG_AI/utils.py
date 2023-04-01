ebfaa56fb6e87c5d098eeae0d4d2bc926a68f058
tkuo-tkuo <nmakerbsiwill@gmail.com>
2023-04-01 08:59:58 +0000

    v0.4.10 move functions into OpenaiUtils class into LNG_AI/utils.py

--- LNG_AI/utils.py
+++ LNG_AI/utils.py
@@ -7,10 +7,27 @@ import random
 import json
 from collections import Counter
 
-from dotenv import load_dotenv
+from datetime import datetime
 from mutagen.mp3 import MP3
+import openai
 
 from LNG_AI import constants
+
+
+class InteractionUtils():
+    """ Interaction utils """
+    @staticmethod
+    def request_continue_permission():
+        """Request permission to continue"""
+        answer = input("Continue? please type 'yes/y' or 'no/n'")
+        if answer.lower() in ["y", "yes"]:
+            return True
+        elif answer.lower() in ["n", "no"]:
+            print("ok, bye")
+            return False  # exit
+        else:
+            print("Invalid input, please type 'yes/y' or 'no/n'")
+            InteractionUtils.request_continue_permission()
 
 
 class FileUtils():
@@ -193,10 +210,9 @@ class JsonlUtils():
 
                         for idx in range(0, len(words) -
                                          num_of_sentences_to_consider):
-                            # Reference: https://platform.openai.com/docs/guides/fine-tuning
-                            # TODO: move separator to constants.py
-                            separator = "/!"
-
+                            # Reference:
+                            # https://platform.openai.com/docs/guides/fine-tuning
+                            separator = constants.SEPARRATOR
                             jsonl = {"prompt": separator.join(words[idx:idx + num_of_sentences_to_consider]),
                                      "completion": words[idx + num_of_sentences_to_consider]}
                             jsonl_dataset_list.append(jsonl)
@@ -207,3 +223,147 @@ class JsonlUtils():
         for portion in portions:
             JsonlUtils.store_portion_jsonl(
                 jsonl_dataset_list, portion)
+
+    @staticmethod
+    def get_jsonls(jsonl_path: str) -> list[dict]:
+        """Read jsonl file"""
+        jsonls = []
+        with open(jsonl_path, "r") as file:
+            for line in file:
+                jsonls.append(json.loads(line))
+        return jsonls
+
+
+class OpenaiUtils():
+    """Class for common openai utilities"""
+    @staticmethod
+    def test_fine_tune_model(model_name: str, num_of_sentences_generated: int):
+        """Test fine-tune model"""
+        assert model_name is not None, "model_name cannot be None"
+        assert num_of_sentences_generated > 0, "num_of_sentences_generated must be > 0"
+
+        print(f"Testing fine-tune model: {model_name}")
+        print(f"Number of sentences generated: {num_of_sentences_generated}")
+
+        # Estimate cost
+        estimated_cost = OpenaiUtils.estimate_cost_estimation(
+            num_of_sentences_generated=num_of_sentences_generated, mode="usage")
+        print(f"Estimated cost: ${estimated_cost}")
+        if not InteractionUtils.request_continue_permission():
+            exit()
+
+        os.makedirs(
+            constants.RootDirectory.GENERATED_FILE_ROOT.value,
+            exist_ok=True)
+
+        # API reference:
+        # https://platform.openai.com/docs/api-reference/completions/create
+        sentences = constants.PROMPT_SENTENCES.copy()
+        chat_history = constants.PROMPT_SENTENCES.copy()
+        for _ in range(num_of_sentences_generated):
+            assert len(sentences) == len(
+                constants.PROMPT_SENTENCES), f"length of prompt sentences must be {len(constants.PROMPT_SENTENCES)}"
+
+            prompt = constants.SEPARRATOR.join(sentences)
+            print(f"Prompt: {prompt}")
+            encoded_prompt = prompt.encode("unicode_escape").decode("utf-8")
+            print(f"Encoded prompt: {encoded_prompt}")
+
+            num_tokens = random.randint(int(constants.AVG_NUM_OF_TOKENS_PER_GENERATED_SENTENCE * 0.8),
+                                        int(constants.AVG_NUM_OF_TOKENS_PER_GENERATED_SENTENCE * 1.2))
+            response = openai.Completion.create(
+                model=model_name,
+                prompt=prompt,
+                max_tokens=num_tokens,
+                presence_penalty=0.2,
+                frequency_penalty=0.2)
+
+            new_sentence = response["choices"][0]["text"]
+            sentences.pop(0)
+            sentences.append(new_sentence)
+            chat_history.append(new_sentence)
+
+        print("Chat history: ", chat_history)
+        generated_chat_history_file_path = os.path.join(
+            constants.RootDirectory.GENERATED_FILE_ROOT.value,
+            f"generated_chat_history_{model_name}_{num_of_sentences_generated}_{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.txt")
+        with open(generated_chat_history_file_path, "w") as chat_history_file:
+            for sentence in chat_history:
+                chat_history_file.write(sentence + "\n")
+
+    @staticmethod
+    def view_training_process(model_id: str):
+        """View training process for a given model_id"""
+        assert model_id is not None, "model_id cannot be None"
+
+        print(f"Viewing training process for model_id: {model_id}")
+        fine_tune_job_retrieve_response = openai.FineTune.retrieve(model_id)
+
+        overall_status = fine_tune_job_retrieve_response["status"]
+        print("==> Overall status: ", overall_status)
+        for event in fine_tune_job_retrieve_response["events"]:
+            eight_hours_in_seconds = 8 * 60 * 60
+            print("==>", datetime.utcfromtimestamp(
+                event["created_at"] + eight_hours_in_seconds), "Event message: ", event["message"])
+
+    @staticmethod
+    def fine_tune(jsonl_dataset_path: str):
+        """
+        Note: currently it is NOT supported to fine-tune from a previous fine-tune model
+        """
+        estimated_cost = OpenaiUtils.estimate_cost_estimation(
+            jsonl_dataset_path=jsonl_dataset_path, mode="train")
+        print(f"Estimated cost: ${estimated_cost}")
+
+        if not InteractionUtils.request_continue_permission():
+            exit()
+
+        with open(jsonl_dataset_path) as jsonl_file:
+            file_create_response = openai.File.create(
+                file=jsonl_file, purpose='fine-tune')
+        file_id = file_create_response["id"]
+
+        print("Start fine-tuning...")
+        response = openai.FineTune.create(
+            training_file=file_id, model='babbage')
+        print(response)
+
+    @staticmethod
+    def view_fine_tune_models():
+        """View all fine-tune models"""
+        fine_tune_models_list = openai.FineTune.list()
+        for fine_tune_model in fine_tune_models_list["data"]:
+            model_name = fine_tune_model["fine_tuned_model"]
+            model_id = fine_tune_model["id"]
+            model_training_file_size_history = [
+                training_file["bytes"] for training_file in fine_tune_model["training_files"]]
+            print(model_name)
+            print(f"==> ID: {model_id}")
+            print(
+                f"==> Training file size history (bytes) {model_training_file_size_history}")
+
+    @staticmethod
+    def estimate_cost_estimation(num_of_sentences_generated: int = 0,
+                                 jsonl_dataset_path: str = "", mode: str = "") -> float:
+        """Estimate cost estimation for a given jsonl dataset"""
+
+        if mode == "train":
+            jsonls = JsonlUtils.get_jsonls(jsonl_dataset_path)
+            estimated_word_count = sum(
+                [len(jsonl["prompt"]) + len(jsonl["completion"]) for jsonl in jsonls])
+            estimated_token_count = estimated_word_count * 2
+            print(f"Estimated token count: {estimated_token_count}")
+            estimated_1k_token_count = estimated_token_count / 1000
+            estimated_cost = estimated_1k_token_count * \
+                constants.OpenaiBabbageCost.TRAINING_PER_1K_TOKENS.value
+            estimated_cost *= 4  # 4 epochs for fine-tuning as default
+            return round(estimated_cost, 4)
+        elif mode == "usage":
+            estimated_token_count = num_of_sentences_generated * \
+                constants.AVG_NUM_OF_TOKENS_PER_GENERATED_SENTENCE
+            estimated_1k_token_count = estimated_token_count / 1000
+            estimated_cost = estimated_1k_token_count * \
+                constants.OpenaiBabbageCost.USAGE_PER_1K_TOKENS.value
+            return estimated_cost
+        else:
+            raise ValueError("Invalid mode")
--- fine_tune_openai_model.py
+++ fine_tune_openai_model.py
@@ -1,9 +1,6 @@
 """Python script for creating jsonl database"""
 import os
-import random
-
 import argparse
-from datetime import datetime
 from dotenv import load_dotenv
 import openai
 
@@ -63,161 +60,17 @@ def main():
     openai.api_key = os.getenv("OPENAI_API_KEY")
 
     if args.mode == constants.OpenaiBabbageModelInteractionMode.FINE_TUNE.value:
-        fine_tune(jsonl_dataset_path=args.jsonl_dataset_path)
+        utils.OpenaiUtils.fine_tune(jsonl_dataset_path=args.jsonl_dataset_path)
     elif args.mode == constants.OpenaiBabbageModelInteractionMode.VIEW_FINE_TUNE_MODELS.value:
-        view_fine_tune_models()
+        utils.OpenaiUtils.view_fine_tune_models()
     elif args.mode == constants.OpenaiBabbageModelInteractionMode.TEST_FINE_TUNE_MODEL.value:
-        test_fine_tune_model(model_name=args.model_name,
-                             num_of_sentences_generated=args.num_of_sentences_generated)
+        utils.OpenaiUtils.test_fine_tune_model(model_name=args.model_name,
+                                               num_of_sentences_generated=args.num_of_sentences_generated)
     elif args.mode == constants.OpenaiBabbageModelInteractionMode.VIEW_TRAINING_PROCESS.value:
-        view_training_process(model_id=args.model_id)
+        utils.OpenaiUtils.view_training_process(model_id=args.model_id)
     else:
         raise ValueError("Invalid mode")
     return 0
-
-
-def test_fine_tune_model(model_name: str, num_of_sentences_generated: int):
-    """Test fine-tune model"""
-    assert model_name is not None, "model_name cannot be None"
-    assert num_of_sentences_generated > 0, "num_of_sentences_generated must be > 0"
-
-    print(f"Testing fine-tune model: {model_name}")
-    print(f"Number of sentences generated: {num_of_sentences_generated}")
-
-    # Estimate cost
-    estimated_cost = estimate_cost_estimation(
-        num_of_sentences_generated=num_of_sentences_generated, mode="usage")
-    print(f"Estimated cost: ${estimated_cost}")
-    if not request_continue_permission():
-        exit()
-
-    os.makedirs(
-        constants.RootDirectory.GENERATED_FILE_ROOT.value,
-        exist_ok=True)
-
-    # API reference:
-    # https://platform.openai.com/docs/api-reference/completions/create
-    sentences = constants.PROMPT_SENTENCES.copy()
-    chat_history = constants.PROMPT_SENTENCES.copy()
-    for _ in range(num_of_sentences_generated):
-        assert len(sentences) == len(
-            constants.PROMPT_SENTENCES), f"length of prompt sentences must be {len(constants.PROMPT_SENTENCES)}"
-
-        prompt = constants.SEPARRATOR.join(sentences)
-        print(f"Prompt: {prompt}")
-        encoded_prompt = prompt.encode("unicode_escape").decode("utf-8")
-        print(f"Encoded prompt: {encoded_prompt}")
-
-        num_tokens = random.randint(int(constants.AVG_NUM_OF_TOKENS_PER_GENERATED_SENTENCE * 0.8),
-                                    int(constants.AVG_NUM_OF_TOKENS_PER_GENERATED_SENTENCE * 1.2))
-        response = openai.Completion.create(
-            model=model_name,
-            prompt=prompt,
-            max_tokens=num_tokens,
-            presence_penalty=0.2,
-            frequency_penalty=0.2)
-
-        new_sentence = response["choices"][0]["text"]
-        sentences.pop(0)
-        sentences.append(new_sentence)
-        chat_history.append(new_sentence)
-
-    print("Chat history: ", chat_history)
-    generated_chat_history_file_path = os.path.join(
-        constants.RootDirectory.GENERATED_FILE_ROOT.value,
-        f"generated_chat_history_{model_name}_{num_of_sentences_generated}_{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.txt")
-    with open(generated_chat_history_file_path, "w") as chat_history_file:
-        for sentence in chat_history:
-            chat_history_file.write(sentence + "\n")
-
-
-def view_training_process(model_id: str):
-    """View training process for a given model_id"""
-    assert model_id is not None, "model_id cannot be None"
-
-    print(f"Viewing training process for model_id: {model_id}")
-    fine_tune_job_retrieve_response = openai.FineTune.retrieve(model_id)
-
-    overall_status = fine_tune_job_retrieve_response["status"]
-    print("==> Overall status: ", overall_status)
-    for event in fine_tune_job_retrieve_response["events"]:
-        eight_hours_in_seconds = 8 * 60 * 60
-        print("==>", datetime.utcfromtimestamp(
-            event["created_at"] + eight_hours_in_seconds), "Event message: ", event["message"])
-
-
-def fine_tune(jsonl_dataset_path: str):
-    """
-    Note: currently it is NOT supported to fine-tune from a previous fine-tune model
-    """
-    estimated_cost = estimate_cost_estimation(
-        jsonl_dataset_path=jsonl_dataset_path, mode="train")
-    print(f"Estimated cost: ${estimated_cost}")
-
-    if not request_continue_permission():
-        exit()
-
-    with open(jsonl_dataset_path) as jsonl_file:
-        file_create_response = openai.File.create(
-            file=jsonl_file, purpose='fine-tune')
-    file_id = file_create_response["id"]
-
-    print("Start fine-tuning...")
-    response = openai.FineTune.create(training_file=file_id, model='babbage')
-    print(response)
-
-
-def view_fine_tune_models():
-    """View all fine-tune models"""
-    fine_tune_models_list = openai.FineTune.list()
-    for fine_tune_model in fine_tune_models_list["data"]:
-        model_name = fine_tune_model["fine_tuned_model"]
-        model_id = fine_tune_model["id"]
-        model_training_file_size_history = [
-            training_file["bytes"] for training_file in fine_tune_model["training_files"]]
-        print(model_name)
-        print(f"==> ID: {model_id}")
-        print(
-            f"==> Training file size history (bytes) {model_training_file_size_history}")
-
-
-def request_continue_permission():
-    """Request permission to continue"""
-    answer = input("Continue? please type 'yes/y' or 'no/n'")
-    if answer.lower() in ["y", "yes"]:
-        return True
-    elif answer.lower() in ["n", "no"]:
-        print("ok, bye")
-        return False  # exit
-    else:
-        print("Invalid input, please type 'yes/y' or 'no/n'")
-        request_continue_permission()
-
-
-def estimate_cost_estimation(num_of_sentences_generated: int = 0,
-                             jsonl_dataset_path: str = "", mode: str = "") -> float:
-    """Estimate cost estimation for a given jsonl dataset"""
-
-    if mode == "train":
-        jsonls = utils.JsonlUtils.get_jsonls(jsonl_dataset_path)
-        estimated_word_count = sum(
-            [len(jsonl["prompt"]) + len(jsonl["completion"]) for jsonl in jsonls])
-        estimated_token_count = estimated_word_count * 2
-        print(f"Estimated token count: {estimated_token_count}")
-        estimated_1k_token_count = estimated_token_count / 1000
-        estimated_cost = estimated_1k_token_count * \
-            constants.OpenaiBabbageCost.TRAINING_PER_1K_TOKENS.value
-        estimated_cost *= 4  # 4 epochs for fine-tuning as default
-        return round(estimated_cost, 4)
-    elif mode == "usage":
-        estimated_token_count = num_of_sentences_generated * \
-            constants.AVG_NUM_OF_TOKENS_PER_GENERATED_SENTENCE
-        estimated_1k_token_count = estimated_token_count / 1000
-        estimated_cost = estimated_1k_token_count * \
-            constants.OpenaiBabbageCost.USAGE_PER_1K_TOKENS.value
-        return estimated_cost
-    else:
-        raise ValueError("Invalid mode")
 
 
 if __name__ == "__main__":
